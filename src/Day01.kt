a87cf5b41195e0e79f770fafaa556f545f57ce12
kevinjeremia <jungzstrumida@gmail.com>
2023-12-28 20:41:28 +0700

    day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,31 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val data = input.joinToString(separator = "\n")
+            .split("\n\n")
+            .map { calories ->
+                calories.lines()
+                    .map { it.toInt() }
+            }
+
+        return data.maxOf { it.sum() }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return input.joinToString(separator = "\n")
+            .split("\n\n")
+            .map { calories ->
+                calories.lines()
+                    .sumOf { it.toInt() }
+            }.sortedDescending()
+            .take(3)
+            .sum()
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    // val testInput = readInput("Day01_test")
+    // check(part1(testInput) == 1)
 
-    val input = readInput("Day01")
+    val input = readInput("input")
     part1(input).println()
     part2(input).println()
 }
